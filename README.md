d363bad265f0581eb067c9c2027713cbbc62ce6a
Tsai Yu Shan <overytsai@gate.sinica.edu.tw>
2022-11-14 11:40:32 +0800

    Week1 - The F2E 活動網站設計 v.1.0 最後更新日期2022-11-14

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# overytsai.github.io
+The F2E 2022 Week1 v.1.0
+=======
+
+### 作品說明：
 Week 1 - The F2E 活動網站設計
+採用邱仲德設計師製作的 UI設計稿
+**  https://www.figma.com/file/eov3MtrljKvUPt2n6ANrOe/The-F2E-%E6%B4%BB%E5%8B%95%E7%B6%B2%E7%AB%99%E8%A8%AD%E8%A8%88?node-id=12%3A1887
+評估後認為需要客製處理的 components 過多，本次尚未使用任何前端框架
+---------
+### 資料夾說明
+1. CSS: 內含字型資料夾，主要樣式、 CSS 動畫暫時都放置於 main.css，後續逐步更新會使用 SASS 撰寫
+2. js： 主要執行的 GSAP 控制項及 RWD 選單相關的 DOM 都寫在 index.js
+
+### 使用技術
+1. jQuery: DOM 好朋友，再戰十年
+2. GSAP： 根據視覺稿及動畫演示的說明，分別使用GSAP Timeline、Tween、ScrollTrigger等動畫
+3. mousemove 事件：視覺稿有提及希望眼球可以跟著滑鼠移動轉動，由於動畫處理佔大多數時間，這部份會於v2.0實作
+4. CSS Animation: 很多閃爍的部份、跑馬燈效果都使用 CSS 動畫
+
+### 第三方服務
+1. GSAP：捲動十載入動畫等互動幾乎倚賴 GSAP 完成
+2. type.js：首頁 Banner 打字動畫效果由 type.js完成
+
+
---
+++ js/index.js
@@ -0,0 +1,259 @@
+$(document).ready(function() {
+	gsap.registerPlugin(ScrollTrigger);
+	//RWD menu
+	$('.rwd-menu').on('click',  function(e) {
+		e.preventDefault();
+		$('.menu-list').toggleClass('active');
+	});
+	
+
+	//typed.js
+
+	var typed = new Typed(".index-title", {
+	    strings: ['The F2E'],
+	    typeSpeed : 150,
+  		elaySpeed : 90,
+	});
+	var typed = new Typed(".sub-title", {
+	    strings: ['互動式網頁設計!'],
+	    typeSpeed : 180,
+	    delaySpeed : 90,
+	});
+
+	//GSAP
+	var titleShinning = function() {		
+		var titleShine = document.getElementById("title-shine");
+		var t3 = new TimelineMax({delay:3});
+		t3.to(titleShine, {duration:.5, alpha: 1});
+	}
+	if ($(window).width() > 960) {
+		// GSAP
+		gsap.fromTo(".fade-in", {x: -90, y:50, autoAlpha: 0 }, {x: 0, y:0, duration: 1.5, autoAlpha: 1, delay:5.8 });
+		gsap.fromTo(".fade-in-more", {x: -90, y:30, autoAlpha: 0 }, {x: 0, y:0, duration: 1.5, autoAlpha: 1, delay:6.3 });
+
+		var lighting = function() {
+		  var light = document.getElementById("light"); //object
+		  var earthgroup = document.getElementById("earth-group"); //object
+		  var lightpoint = document.getElementById("light-point-group");
+		  var tl = new TimelineMax({delay:2.5, repeat: 3});
+		  
+		  tl.to(light, {duration:.06, alpha: 0, delay: .2})
+		    .to(light, {duration:.1, alpha: 1});
+
+		  var t2 = new TimelineMax({delay:3.35});
+		  
+		  t2.to(earthgroup, {alpha: 0})
+		  	.to(earthgroup, {alpha: .4})
+		    .to(earthgroup, {duration:.75, alpha: 1})
+		    .to(lightpoint, {duration:.2,x: 300, y:800, scale: 0, alpha: 0})
+		    .to(lightpoint, {duration:.5,x: 0, y:0, scale: 1, alpha: 1});
+		}
+		lighting()
+		
+	}
+	else {
+	   titleShinning()
+	};
+	var painPoint = function() {
+		var block = gsap.utils.toArray('.section-block2');
+
+		gsap.set('.pain-img', {xPercent:-50,autoAlpha: 0})
+		gsap.set('.pain-mask', {autoAlpha: 0})
+
+		var paintl = gsap.timeline({
+		  scrollTrigger: { 
+		    trigger: block,
+		    pin: block,
+		    scrub: 0.5,
+		    start: "top top",
+		    end: "+=50px"
+		  },
+		})
+
+		paintl
+		.to('.pp01 .pain-mask', { autoAlpha: 1, duration: .7 }) 
+		.to({}, {duration: 1 }) // a little pause in between
+		.to('.pp01 .pain-img', { xPercent: 0, autoAlpha: 1, duration: .5, ease:'power2.inOut' })
+
+		.to({}, {duration: 2 }) // a little pause in between
+
+		.to('.pp02 .pain-mask', { autoAlpha: 1, duration: .7 }) 
+		.to({}, {duration: 1 }) // a little pause in between
+		.to('.pp02 .pain-img', { xPercent: 0, autoAlpha: 1, duration: .5, ease:'power2.inOut' })
+
+		.to({}, {duration: 2 }) // a little pause in between
+
+		.to('.pp03 .pain-mask', { autoAlpha: 1, duration: .7 }) 
+		.to({}, {duration: 1 }) // a little pause in between
+		.to('.pp03 .pain-img', { xPercent: 0, autoAlpha: 1, duration: .5, ease:'power2.inOut' })
+
+		 // a little pause in between
+	}
+
+	var dealLoading = function() {
+		var dealtl = new TimelineMax({ease:'power2.inOut'});
+
+		dealtl
+		.from('.deal-ui', { xPercent: -100, autoAlpha: 0, duration: .25 })
+		.to('.deal-ui', {duration: .5 })
+		.from('.deal-f2e', { xPercent: 100, autoAlpha: 0, duration: .25 })
+		.to('.deal-f2e', {duration: .5 })
+		.from('.deal02', { xPercent: 10, scale: 0, autoAlpha: 0, duration: .3 })
+		.from('.deal03', { xPercent: -10, scale: 0, autoAlpha: 0, duration: .3 })
+		.to({}, {duration: .5 })
+		.from('.deal01', { xPercent: 10, scale: 0, autoAlpha: 0, duration: .4 })
+		.from('.deal04', { xPercent: -10, scale: 0, autoAlpha: 0, duration: .4 })
+		.from('.deal-talk', { yPercent: 30, scale: 0, autoAlpha: 0, duration: .5, delay: .2 })
+
+		ScrollTrigger.create({
+		  trigger: ".section-block3",
+		  start: "top 100px",
+		  endTrigger: ".section-block4",
+		  animation: dealtl,
+		});
+	}
+
+	var fadeinTwoSide = function() {
+		var small = gsap.utils.toArray('.schedule-small');
+		var large = gsap.utils.toArray('.schedule-large');
+
+		gsap.set('.schedule-small', {xPercent:50,autoAlpha: 0})
+		gsap.set('.schedule-large', {xPercent:-120,autoAlpha: 0})
+
+		var righttl = gsap.timeline({
+		  scrollTrigger: { 
+		    trigger: small,
+		    // pin: true,
+		    scrub: 0.5,
+		    start: "top center",
+		    end: "+=500px",
+		    scrub: true
+		  },
+		})
+
+		righttl
+		.to(small, { xPercent: 0, autoAlpha: 1, duration: 5, ease:'power2.inOut', delay: 1 })
+
+		var lefttl = gsap.timeline({
+		  scrollTrigger: { 
+		    trigger: large,
+		    // pin: true,
+		    scrub: 0.5,
+		    start: "top center+=100",
+		    end: "+=300px",
+		    scrub: true
+		  },
+		})
+
+		lefttl
+		.to(large, { xPercent: 0, autoAlpha: 1, duration: 8, ease:'power2.inOut' })
+
+	}
+
+	var windowShinning = function() {
+		gsap.to(".wall-block", {
+		  yPercent: -50,
+		  ease: "none",
+		  scrollTrigger: {
+		    trigger: ".section-block2",
+		    scrub: true
+		  }, 
+		});
+	}
+	//about three topic
+	var topicTV = function() {
+		gsap.registerPlugin(ScrollTrigger);
+		/* Main navigation */
+		let topicsSection = document.querySelector(".section-block4"),
+			  topicsContainer = document.querySelector("#topics-container"),
+		    anchors = gsap.utils.toArray(".anchor"),
+			  tween;
+
+
+		/* Panels */
+		const panels = gsap.utils.toArray("#topics-container .topic-panel");
+		const snap = 1 / (panels.length - 1);
+		let activeNav;
+
+		tween = gsap.to(panels, {
+			xPercent: -100 * ( panels.length - 1 ),
+			ease: "none",
+			scrollTrigger: {
+				trigger: "#topics-container",
+				pin: true,
+				start: "top top",
+				scrub: 1,
+				snap: {
+					snapTo: 1 / (panels.length - 1),
+					inertia: false,
+					duration: {min: 0.1, max: 0.1}
+				},
+				end: () => "+=" + (topicsContainer.offsetWidth - innerWidth)
+			}
+		});
+
+		anchors.forEach((anchor, i) => {
+		  let targetElem = document.querySelector(anchor.getAttribute("href"));
+			anchor.addEventListener("click", function(e) {
+				e.preventDefault();
+				let y = targetElem;
+				if (targetElem && topicsContainer.isSameNode(targetElem.parentElement)) {
+					let totalScroll = tween.scrollTrigger.end - tween.scrollTrigger.start,
+						totalMovement = (panels.length - 1) * targetElem.offsetWidth;
+					y = Math.round(tween.scrollTrigger.start + (targetElem.offsetLeft / totalMovement) * totalScroll);
+				}
+				gsap.to(window, {
+					scrollTo: {
+						y: y,
+						autoKill: false
+					},
+					duration: 1
+				});
+			});
+		});
+		function getScrollLookup(targets, position, containerAnimation) {
+		  let triggers = gsap.utils.toArray(targets).map(el => ScrollTrigger.create({
+		        trigger: el,
+		        start: position || "top top",
+		        refreshPriority: -10,
+		        containerAnimation: containerAnimation
+		      })),
+		      st = containerAnimation && containerAnimation.scrollTrigger;
+		  return target => {
+		    let t = gsap.utils.toArray(target)[0],
+		        i = triggers.length;
+		    while (i-- && triggers[i].trigger !== t) {};
+		    if (i < 0) {
+		      return console.warn("target not found", target);
+		    } 
+		    return containerAnimation ? st.start + (triggers[i].start / containerAnimation.duration()) * (st.end - st.start) : triggers[i].start;
+		  };
+		}
+	}
+
+	//QA tab page 
+	$('#tabs-nav > li > a').click(function(event){
+		event.preventDefault();
+		var active_tab = $('#tabs-nav > li.active > a').attr('href');
+		//find actived navigation and remove 'active' css
+		var actived_nav = $('#tabs-nav > li.active');
+		actived_nav.removeClass('active');
+						
+		//add 'active' css into clicked navigation
+		$(this).parents('li').addClass('active');
+						
+		//hide displaying tab content
+		$(active_tab).removeClass('active');
+		$(active_tab).addClass('hide');
+						
+		//show target tab content
+		var target_tab = $(this).attr('href');
+		$(target_tab).removeClass('hide');
+		$(target_tab).addClass('active');
+	});
+	painPoint()
+	windowShinning()
+	dealLoading()
+	topicTV()
+	fadeinTwoSide()
+});
